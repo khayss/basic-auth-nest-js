60fa6ca0415efc6354dee7c3dd69d0bfd1a18406
KayPrime <forendals@gmail.com>
2024-07-17 05:58:38 +0100

    update: migrations

--- prisma/migrations/0_init/migration.sql
+++ prisma/migrations/0_init/migration.sql
@@ -2,16 +2,18 @@
 CREATE TYPE "EmploymentStatus" AS ENUM ('EMPLOYED', 'UNEMPLOYED', 'STUDENT', 'RETIRED');
 
 -- CreateTable
-CREATE TABLE "user" (
+CREATE TABLE "AssimUser" (
     "id" TEXT NOT NULL,
     "firstName" TEXT NOT NULL,
     "lastName" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
+    "phoneNumber" TEXT NOT NULL,
     "employmentStatus" "EmploymentStatus" NOT NULL,
 
-    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "AssimUser_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+CREATE UNIQUE INDEX "AssimUser_email_key" ON "AssimUser"("email");
+
